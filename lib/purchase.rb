893af10cc43c39220d12f252e92f7228aa929f24
Diego Suarez and Margie Nevarez <diego@>
2016-12-13 11:01:12 -0800

    before sinatra

--- lib/purchase.rb
+++ lib/purchase.rb
@@ -1,16 +1,19 @@
-require('./lib/product')
+
 class Purchase < ActiveRecord::Base
   belongs_to :customers
   belongs_to :products
 
 
-  define_singleton_method(:calculate_price) do |id, date|
+  define_singleton_method(:calculate_price) do |id, start_date, end_date|
     total_cost = 0
-    purchases = Purchase.where("customer_id = #{id} AND date = '#{date}'")
+    purchases = Purchase.where("customer_id = #{id} AND date >= '#{start_date}' AND date <= '#{end_date}'")
     purchases.each() do |purchase|
       total_cost+= (purchase.quantity * purchase.price)
     end
     total_cost
   end
 
+  define_singleton_method(:purchases_by_date) do |start_date, end_date|
+    purchases = Purchase.where("date >= '#{start_date}' AND date <= '#{end_date}'")
+  end
 end
--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -10,7 +10,30 @@ describe(Purchase) do
       test_purchase1 = Purchase.create({:date => "2016-12-12", :product_id => test_product.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product.price})
       test_purchase2 = Purchase.create({:date => "2016-12-12", :product_id => test_product2.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product2.price})
       test_purchase3 = Purchase.create({:date => "2016-12-12", :product_id => test_product3.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product3.price})
-      expect(Purchase.calculate_price(test_customer.id, '2016-12-12')).to eq(16.98)
+      expect(Purchase.calculate_price(test_customer.id, '2016-12-12', '2016-12-12')).to eq(16.98)
     end
+    it "returns purchases by date" do
+      test_product = Product.create({:name => "milk", :price => 2.3, :inventory => 56, :description => "it's 2 percent milk"})
+      test_product2 = Product.create({:name => "protein", :price => 4.2, :inventory => 40, :description => "20 grams of protein"})
+      test_product3 = Product.create({:name => "hot sauce", :price => 1.99, :inventory => 31, :description => "its super spicy"})
+      test_customer = Customer.create({:name => "Margie", :email => "email@gmail.com"})
+      test_purchase1 = Purchase.create({:date => "2016-12-12", :product_id => test_product.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product.price})
+      test_purchase2 = Purchase.create({:date => "2016-12-14", :product_id => test_product2.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product2.price})
+      test_purchase3 = Purchase.create({:date => "2016-12-23", :product_id => test_product3.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product3.price})
+      expect(Purchase.purchases_by_date('2016-12-12', '2016-12-16')).to eq([test_purchase1, test_purchase2])
+    end
+  # it "returns purchases by date" do
+  #   test_product = Product.create({:name => "milk", :price => 2.3, :inventory => 56, :description => "it's 2 percent milk"})
+  #   test_product2 = Product.create({:name => "protein", :price => 4.2, :inventory => 40, :description => "20 grams of protein"})
+  #   test_product3 = Product.create({:name => "hot sauce", :price => 1.99, :inventory => 31, :description => "its super spicy"})
+  #   test_customer = Customer.create({:name => "Margie", :email => "email@gmail.com"})
+  #   test_purchase1 = Purchase.create({:date => "2016-12-12", :product_id => test_product.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product.price})
+  #   test_purchase2 = Purchase.create({:date => "2016-12-14", :product_id => test_product2.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product2.price})
+  #   test_purchase3 = Purchase.create({:date => "2016-12-23", :product_id => test_product3.id, :customer_id => test_customer.id, :quantity => 2, :price => test_product3.price})
+  #   Purchase.checkout(test_customer.id, "2016-12-12")
+  #   expect(test_product.inventory.to eq(54)
+  #   expect(.to eq(54)
+  #
+  # end
   end
 end
